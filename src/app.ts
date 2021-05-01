d3470f7677f7b9ebef54f972f76a39e841b75eb9
trickypr <trickypr@icloud.com>
2021-05-02 05:46:48 +1000

    ✨ Allow for multiple meml files at once

--- src/app.ts
+++ src/app.ts
@@ -1,28 +1,32 @@
-import { command, run, string, positional } from 'cmd-ts'
+import { command, run, string, array, positional, multioption } from 'cmd-ts'
 import { readFileSync, writeFileSync } from 'fs'
 import { MemlC } from './core'
 
 const cmd = command({
   name: 'memlc',
-  description: 'The core meml transpiler',
+  description: 'The fushra meml transpiler core',
   version: '1.0.0',
   args: {
-    file: positional({
-      type: string,
-      displayName: 'file',
+    file: multioption({
+      type: array(string),
+      long: 'file',
       description: 'The path to your meml file',
     }),
   },
   handler: (args) => {
     console.time('Compile time')
-    const { file } = args
-    const out = file.replace('.meml', '.html')
 
-    const meml = readFileSync(file).toString()
+    args.file.forEach((file) => {
+      const out = file.replace('.meml', '.html')
+      const meml = readFileSync(file).toString()
+      const c = new MemlC()
+      writeFileSync(out, c.translate(meml, file))
+    })
 
-    const c = new MemlC()
+    if (args.file.length == 0) {
+      console.log('--help for list of commands')
+    }
 
-    writeFileSync(out, c.translate(meml, file))
     console.timeEnd('Compile time')
   },
 })
