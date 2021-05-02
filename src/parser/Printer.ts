5a25bb49ca11fd81e36cb09d12a378dea5e9d10b
trickypr <trickypr@icloud.com>
2021-05-02 17:56:06 +1000

    ✨ Update ast printer

--- src/parser/Printer.ts
+++ src/parser/Printer.ts
@@ -3,18 +3,29 @@ import {
   GroupingExpr,
   IExpr,
   LiteralExpr,
-  PageExpr,
-  TagExpr,
   UnaryExpr,
-  Visitor,
+  ExprVisitor,
+  DestructureExpr,
+  IdentifierExpr,
+  MemlPropertiesExpr,
 } from './Expr'
+import {
+  ComponentStmt,
+  ExportStmt,
+  ExpressionStmt,
+  ImportStmt,
+  IStmt,
+  MemlStmt,
+  PageStmt,
+  StmtVisitor,
+} from './Stmt'
 
-export class AstPrinter implements Visitor<string> {
-  print(expr: IExpr): string {
+export class AstPrinter implements ExprVisitor<string>, StmtVisitor<string> {
+  print(expr: IExpr | IStmt): string {
     return expr.accept(this)
   }
 
-  private parenthesize(name: string, ...exprs: IExpr[]): string {
+  private parenthesize(name: string, ...exprs: (IExpr | IStmt)[]): string {
     let final = `(${name}`
 
     exprs.forEach((expr) => {
@@ -23,6 +34,51 @@ export class AstPrinter implements Visitor<string> {
     final += ')'
 
     return final
+  }
+
+  visitMemlPropertiesExpr(expr: MemlPropertiesExpr): string {
+    return this.parenthesize('Properties ' + expr.name, expr.value)
+  }
+
+  visitDestructureExpr(expr: DestructureExpr): string {
+    return this.parenthesize(
+      'Destructure ' + expr.items.map((item) => `${item.literal},`)
+    )
+  }
+
+  visitIdentifierExpr(expr: IdentifierExpr): string {
+    return this.parenthesize('Identifier ' + expr.token.literal)
+  }
+
+  visitComponentStmt(stmt: ComponentStmt): string {
+    return this.parenthesize('Component ' + stmt.tagName, stmt.props, stmt.meml)
+  }
+
+  visitExportStmt(stmt: ExportStmt): string {
+    return this.parenthesize('Export', stmt.exports)
+  }
+
+  visitImportStmt(stmt: ImportStmt): string {
+    return this.parenthesize(
+      'Import from ' + stmt.file,
+      (stmt.imports as unknown) as IExpr
+    )
+  }
+
+  visitMemlStmt(stmt: MemlStmt): string {
+    return this.parenthesize(
+      `MEML ${stmt.tagName}`,
+      (stmt.props as unknown) as IExpr,
+      ...stmt.exprOrMeml
+    )
+  }
+
+  visitExpressionStmt(stmt: ExpressionStmt): string {
+    return this.parenthesize('expression', stmt.expression)
+  }
+
+  visitPageStmt(stmt: PageStmt): string {
+    return this.parenthesize('page', ...stmt.children)
   }
 
   visitBinaryExpr(expr: BinaryExpr): string {
@@ -40,13 +96,5 @@ export class AstPrinter implements Visitor<string> {
 
   visitUnaryExpr(expr: UnaryExpr): string {
     return this.parenthesize(expr.operator.lexeme, expr.right)
-  }
-
-  visitTagExpr(expr: TagExpr): string {
-    return this.parenthesize(expr.name.literal, ...expr.right)
-  }
-
-  visitPageExpr(expr: PageExpr): string {
-    return this.parenthesize('Page', ...expr.children)
   }
 }
