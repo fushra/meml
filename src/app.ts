ca6db2ead4bb158065fe2ad5dc2accdfe1bd673b
trickypr <trickypr@icloud.com>
2021-06-01 17:56:12 +1000

    ➕ Migrate to commander for cli

--- src/app.ts
+++ src/app.ts
@@ -1,63 +1,58 @@
-import { command, run, string, array, option, multioption } from 'cmd-ts'
-import { readFileSync, writeFileSync } from 'fs'
+import { program } from 'commander'
+import { writeFileSync } from 'fs'
 import { join, resolve } from 'path'
 import { MemlCore } from './core'
 
 const currentDir = process.cwd()
 
-const cmd = command({
-  name: 'memlc',
-  description: 'The fushra meml transpiler core',
-  version: '1.0.0',
-  args: {
-    file: multioption({
-      type: array(string),
-      long: 'file',
-      short: 'f',
-      description: 'The path to your meml file',
-    }),
-    src: option({
-      type: {
-        ...string,
-        defaultValue: () => './',
-        defaultValueIsSerializable: true,
-      },
-      long: 'src',
-      short: 's',
-      description: 'A root path that the compiler will use rather',
-    }),
-    out: option({
-      type: {
-        ...string,
-        defaultValue: () => './',
-        defaultValueIsSerializable: true,
-      },
-      long: 'out',
-      short: 'o',
-      description:
-        'Where the compiled outputs will be written. Best used with --src',
-    }),
-  },
-  handler: async (args) => {
-    console.time('Compile time')
+program
+  .version('1.0.0')
+  .description(
+    'A basic cli transpiler for meml. For larger projects it is recommended to use "mld"'
+  )
 
-    const src = resolve(currentDir, args.src)
-    const out = resolve(currentDir, args.out)
+program
+  .option('--production', 'Optimise for production')
+  .option(
+    '-f, --file <files...>',
+    'The path to the meml files you wish to compile'
+  )
+  .option(
+    '-s, --src <folder>',
+    'The root source path the compiler will use',
+    './'
+  )
+  .option(
+    '-o, --out <folder>',
+    'The location for the compiled output. It is recommended to use this with --src',
+    './'
+  )
 
-    for (const file of args.file) {
-      const realPath = join(src, file)
-      const realOut = join(out, file.replace('.meml', '.html'))
+program.parse(process.argv)
 
-      const c = new MemlCore()
-      writeFileSync(realOut, await c.fileToWeb(realPath))
-    }
+// This is the main loop for the CLI. This comment is necessary because prettier
+;(async () => {
+  console.time('Compile time')
 
-    if (args.file.length == 0) {
-      console.log('--help for list of commands')
-    }
+  const args = program.opts()
+  const src = resolve(currentDir, args.src)
+  const out = resolve(currentDir, args.out)
 
-    console.timeEnd('Compile time')
-  },
-})
+  if (args.production) {
+    MemlCore.isProduction = true
+  }
 
-run(cmd, process.argv.slice(2))
+  for (const file of args.file) {
+    const realPath = join(src, file)
+    const realOut = join(out, file.replace('.meml', '.html'))
+
+    const c = new MemlCore()
+    writeFileSync(realOut, await c.fileToWeb(realPath))
+  }
+
+  if (args.file.length == 0) {
+    console.log('--help for list of commands')
+  }
+
+  console.timeEnd('Compile time')
+})()
