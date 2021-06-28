f7cc2073ff77c4ff6202cabb80d5ec2f48318e9e
trickypr <trickypr@icloud.com>
2021-06-28 17:32:28 +1000

    ✅ Add tests for path and fs implemementation

--- src/app.test.ts
+++ src/app.test.ts
@@ -1,9 +1,20 @@
 import { expect, TestSuite, Test } from 'testyts'
+import { fs, path } from './fs'
 
 @TestSuite()
 export class TestTheTests {
   @Test()
   true() {
     expect.toBeEqual(true, true)
+  }
+
+  @Test('Janky FS')
+  fs() {
+    expect.toBeDefined(fs)
+  }
+
+  @Test('Janky path')
+  path() {
+    expect.toBeDefined(path)
   }
 }
