880d655fb607427c2396cf6ef688e2a15d00cfc7
trickypr <trickypr@icloud.com>
2021-07-26 18:01:30 +1000

    🔥 Remove web replacement for fs

--- src/fs.ts
+++
@@ -1,9 +0,0 @@
-/// <reference path="./fs.d.ts" />
-
-import isBrowser from 'is-browser'
-import { fs as memfs } from 'memfs'
-import pathBrowserify from 'path.js'
-
-// This needs to run in the browser and node, so this is the way we are dealing with fs
-export const fs = isBrowser ? memfs : require('fs')
-export const path = isBrowser ? pathBrowserify : require('path')
